c3349b1001e6ff337f7f4a933ce11dbe9dcb9639
Gonzalo Perea <gonzalopereabartra@gmail.com>
2022-05-05 10:37:10 -0500

    Automatic Library Import System
    Automatically adds missing libraries. To add to this process, add name in variable "required".

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import sys
+import subprocess
+import pkg_resources
 from os.path import exists as file_exists
+
+#Installing pip packages
+required = {'myconfig'}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+
+if missing:
+    print("Detected missing libraries, installing them using pip.")
+    print("If this fails, check pip is installed and updated.")
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
 
 #Create starting game variables
 
@@ -9,7 +23,7 @@ while True:
     #Check for previous save file.
     if file_exists("saves.txt") == True:
         pass
-        with open("saves.txt", 'r') as reader:
+        #with open("saves.txt", 'r') as reader:
 
     elif file_exists("saves.txt") == False:
         print("Save file not found, initializing fresh save.")
