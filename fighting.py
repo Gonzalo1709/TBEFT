5055902973402f6aae51dd710db5ac8baad26fa7
Gonzalo Perea <gonzalopereabartra@gmail.com>
2022-05-12 19:04:32 -0500

    Added close shots and fixed inconsistencies

--- fighting.py
+++ fighting.py
@@ -17,61 +17,64 @@ def fight(enemy, kindofencounter):
         print("They saw you first. It's their turn!")
         turn = "them"
 
-
-
-    while myhealth.head > 0 and myhealth.thorax > 0 and enemy.health.head > 0 and enemy.health.thorax > 0:
+    def firingsequence():
         aimpoint = 0
         tofire = int(inv["gun"].rof/100)
-        lap = "-" * aimpoint
-        rap = "-"*(20-aimpoint)
-        print("         v            Fire with Ctrl+C", end="\n")
-        print("--------------------", end="\r")
+        print("         v           Fire with Ctrl+C", end="\n")
+        print("-------------------", end="\r")
         speedofcursor = 0.3-(tofire/40)
         direction = True
         hits = 0
+        close = 0
         while tofire > 0:
             try:
                 while True:
                     if direction == True:
                         direction2 = False
                         while True:
-                            if aimpoint == 20 or aimpoint == 0:
+                            if aimpoint == 19 or aimpoint == 0:
                                 direction2 = not direction2
                             if direction2 == True:
                                 aimpoint += 1
                             else:
                                 aimpoint -= 1
                             lap = "-" * aimpoint
-                            rap = "-" * (20-aimpoint)
-                            fullaim = lap + "×" + rap + " On point: " + str(hits) + " Left: " + str(tofire)
+                            rap = "-" * (19-aimpoint)
+                            fullaim = lap + "×" + rap + " On point: " + str(hits) + " Close: " + str(close) + " Left: " + str(tofire)
                             print(fullaim, end="\r")
                             time.sleep(speedofcursor)
                     else:
                         direction2 = True
                         while True:
-                            if aimpoint == 20 or aimpoint == 0:
+                            if aimpoint == 19 or aimpoint == 0:
                                 direction2 = not direction2
                             if direction2 == True:
                                 aimpoint += 1
                             else:
                                 aimpoint -= 1
                             lap = "-" * aimpoint
-                            rap = "-" * (20-aimpoint)
-                            fullaim = lap + "×" + rap + " On point: " + str(hits) + " Left: " + str(tofire)
+                            rap = "-" * (19-aimpoint)
+                            fullaim = lap + "×" + rap + " On point: " + str(hits) + " Close: " + str(close) + " Left: " + str(tofire)
                             print(fullaim, end="\r")
                             time.sleep(speedofcursor)
 
             except KeyboardInterrupt:
-                if aimpoint == 10 or aimpoint == 9 or aimpoint == 11:
+                if aimpoint == 9:
                     hits += 1
+                if aimpoint == 8 or aimpoint == 10:
+                    close =+ 1
                 tofire -= 1
                 direction = not direction
                 if direction == True:
                     aimpoint = 0
                 else:
-                    aimpoint = 20
+                    aimpoint = 19
                 pass
-        fullaim = lap + "×" + rap + " On point: " + str(hits) + " Left: " + str(tofire) + "" + str(aimpoint)
+        fullaim = lap + "×" + rap + " On point: " + str(hits) + " Close: " + str(close) + " Left: " + str(tofire)
         print(fullaim, end="\r")
         print("")
-        print(f"You shot {hits} shots on point!")
+        print(f"You shot {hits} shots on point and {close} close!")
+        return(hits, close)
+
+    while myhealth.head > 0 and myhealth.thorax > 0 and enemy.health.head > 0 and enemy.health.thorax > 0:
+        print()
